10bb879bfe31c184bc806f5ddf65df8bfc9000e9
Haroon <mharoonbutt13@gmail.com>
2022-12-06 08:13:02 +0000

    updated files

---
+++ assets/js/myjs.js
@@ -0,0 +1,6 @@
+// let errordiv = select('.error-message')
+// on('click', '.button', function(e) {
+//     errordiv.classList.remove('d-block');
+//     document.getElementById("smsg").style.display = 'block';
+
+//   }, true)
